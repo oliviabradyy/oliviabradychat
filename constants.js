178f195bf8b017843ff55d2585df3e31998b3ab0
oliviabradyy <92897530+oliviabradyy@users.noreply.github.com>
2021-10-31 19:38:56 +1300

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,27 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["yes", "YES"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello! Are you worried you might have covid? Please type YES if you do"],
+  ["Ok we will do some checks!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
